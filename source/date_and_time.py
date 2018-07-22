390b906034b38c43c165553eea804d94f8369965
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-07-22 14:10:09 -0400

    Added comment

--- source/date_and_time.py
+++ source/date_and_time.py
@@ -1,9 +1,4 @@
-#from datetime import date
-'''import datetime
-current_date = datetime.datetime.today().strftime('%Y-%m-%d')
-current_time = datetime.datetime.time
-print(current_date)
-print(current_time)'''
+# Retrieves the current date and time
 
 from datetime import datetime
 dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S').split(' ')
